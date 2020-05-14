072c91c2b5f092678eec232a202d02d3f9c328f6
Leo <leo.80@protonmail.com>
2020-05-14 14:25:25 -0300

    challenge refactoring

--- module_1/challenge/main.py
+++ module_1/challenge/main.py
@@ -1,31 +1,45 @@
 from datetime import datetime
 
 records = [
-    {'source': '48-996355555', 'destination': '48-666666666', 'end': 1564610974, 'start': 1564610674},
-    {'source': '41-885633788', 'destination': '41-886383097', 'end': 1564506121, 'start': 1564504821},
-    {'source': '48-996383697', 'destination': '41-886383097', 'end': 1564630198, 'start': 1564629838},
-    {'source': '48-999999999', 'destination': '41-885633788', 'end': 1564697158, 'start': 1564696258},
-    {'source': '41-833333333', 'destination': '41-885633788', 'end': 1564707276, 'start': 1564704317},
-    {'source': '41-886383097', 'destination': '48-996384099', 'end': 1564505621, 'start': 1564504821},
-    {'source': '48-999999999', 'destination': '48-996383697', 'end': 1564505721, 'start': 1564504821},
-    {'source': '41-885633788', 'destination': '48-996384099', 'end': 1564505721, 'start': 1564504821},
-    {'source': '48-996355555', 'destination': '48-996383697', 'end': 1564505821, 'start': 1564504821},
-    {'source': '48-999999999', 'destination': '41-886383097', 'end': 1564610750, 'start': 1564610150},
-    {'source': '48-996383697', 'destination': '41-885633788', 'end': 1564505021, 'start': 1564504821},
-    {'source': '48-996383697', 'destination': '41-885633788', 'end': 1564627800, 'start': 1564626000}
+    {'source': '48-996355555', 'destination': '48-666666666',
+     'end': 1564610974, 'start': 1564610674},
+    {'source': '41-885633788', 'destination': '41-886383097',
+     'end': 1564506121, 'start': 1564504821},
+    {'source': '48-996383697', 'destination': '41-886383097',
+     'end': 1564630198, 'start': 1564629838},
+    {'source': '41-833333333', 'destination': '41-885633788',
+     'end': 1564707276, 'start': 1564704317},
+    {'source': '48-999999999', 'destination': '41-885633788',
+     'end': 1564697158, 'start': 1564696258},
+    {'source': '41-886383097', 'destination': '48-996384099',
+     'end': 1564505621, 'start': 1564504821},
+    {'source': '48-999999999', 'destination': '48-996383697',
+     'end': 1564505721, 'start': 1564504821},
+    {'source': '41-885633788', 'destination': '48-996384099',
+     'end': 1564505721, 'start': 1564504821},
+    {'source': '48-996355555', 'destination': '48-996383697',
+     'end': 1564505821, 'start': 1564504821},
+    {'source': '48-999999999', 'destination': '41-886383097',
+     'end': 1564610750, 'start': 1564610150},
+    {'source': '48-996383697', 'destination': '41-885633788',
+     'end': 1564505021, 'start': 1564504821},
+    {'source': '48-996383697', 'destination': '41-885633788',
+     'end': 1564627800, 'start': 1564626000}
 ]
 
 PERMANENT_FEE = 0.36
+MINUTE_FEE = 0.09
 
 
 def classify_by_phone_number(records):
     final_bill = []
     for record in records:
-        initial_hour = int(datetime.fromtimestamp(record['start']).strftime('%H'))
+        call_initial_hour = int(datetime.fromtimestamp(record['start'])
+                                        .strftime('%H'))
         total_minutes = (record['end'] - record['start']) // 60
         total_bill = PERMANENT_FEE
-        if 6 <= initial_hour <= 22:
-            total_bill += (total_minutes * 0.09)
+        if 6 <= call_initial_hour <= 22:
+            total_bill += (total_minutes * MINUTE_FEE)
 
         repeated_source = False
         for bill in final_bill:
@@ -35,7 +49,8 @@ def classify_by_phone_number(records):
                 break
 
         if not repeated_source:
-            final_bill.append({'source': record['source'], 'total': round(total_bill, 2)})
+            final_bill.append({'source': record['source'],
+                               'total': round(total_bill, 2)})
 
     # Ordering the list by total value
     final_bill = sorted(final_bill, key=lambda x: x['total'], reverse=True)
